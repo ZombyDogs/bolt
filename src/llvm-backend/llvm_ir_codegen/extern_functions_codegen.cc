1fcb616bf269181b0d93ef3346849ec9761ebeeb
mukul-rathi <mukul.rathi@gmail.com>
2020-12-25 12:08:17 +0000

    Fix Pthread type defns

--- src/llvm-backend/llvm_ir_codegen/extern_functions_codegen.cc
+++ src/llvm-backend/llvm_ir_codegen/extern_functions_codegen.cc
@@ -23,8 +23,10 @@ void IRCodegenVisitor::codegenExternFunctionDeclarations() {
 
   // PTHREADS
 
+  // a pthread is represented as some opaque struct * in MacOS
   llvm::Type *pthreadTy =
-      llvm::StructType::create(*context, llvm::StringRef("pthread_t"));
+      llvm::StructType::create(*context, llvm::StringRef("pthread_t"))
+          ->getPointerTo();
 
   llvm::Type *pthreadPtrTy = pthreadTy->getPointerTo();
 
@@ -33,13 +35,13 @@ void IRCodegenVisitor::codegenExternFunctionDeclarations() {
       voidPtrTy, llvm::ArrayRef<llvm::Type *>({voidPtrTy}),
       /* has variadic args */ false);
 
-  // int pthread_create(pthread_t ** thread, const pthread_attr_t * attr,
+  // int pthread_create(pthread_t * thread, const pthread_attr_t * attr,
   //                  void * (*start_routine)(void *), void * arg)
   // we can use a void * in place of the opaque platform-specific
   // pthread_attr_t *
   llvm::FunctionType *pthreadCreateTy = llvm::FunctionType::get(
       llvm::Type::getInt32Ty(*context),
-      llvm::ArrayRef<llvm::Type *>({pthreadPtrTy->getPointerTo(), voidPtrTy,
+      llvm::ArrayRef<llvm::Type *>({pthreadPtrTy, voidPtrTy,
                                     (funVoidPtrVoidPtrTy)->getPointerTo(),
                                     voidPtrTy}),
       /* has variadic args */ false);
@@ -48,20 +50,20 @@ void IRCodegenVisitor::codegenExternFunctionDeclarations() {
   // int pthread_join(pthread_t thread, void **value_ptr)
   llvm::FunctionType *pthreadJoinTy = llvm::FunctionType::get(
       llvm::Type::getInt32Ty(*context),
-      llvm::ArrayRef<llvm::Type *>({pthreadPtrTy, voidPtrTy->getPointerTo()}),
+      llvm::ArrayRef<llvm::Type *>({pthreadTy, voidPtrTy->getPointerTo()}),
       /* has variadic args */ false);
   module->getOrInsertFunction("pthread_join", pthreadJoinTy);
 
   // int pthread_equal (pthread_t t1, pthread_t t2);
   llvm::FunctionType *pthreadEqualTy = llvm::FunctionType::get(
       llvm::Type::getInt32Ty(*context),
-      llvm::ArrayRef<llvm::Type *>({pthreadPtrTy, pthreadPtrTy}),
+      llvm::ArrayRef<llvm::Type *>({pthreadTy, pthreadTy}),
       /* has variadic args */ false);
   module->getOrInsertFunction("pthread_equal", pthreadEqualTy);
 
   // pthread_t pthread_self ();
   llvm::FunctionType *pthreadSelfTy =
-      llvm::FunctionType::get(pthreadPtrTy, llvm::ArrayRef<llvm::Type *>({}),
+      llvm::FunctionType::get(pthreadTy, llvm::ArrayRef<llvm::Type *>({}),
                               /* has variadic args */ false);
   module->getOrInsertFunction("pthread_self", pthreadSelfTy);
 }
